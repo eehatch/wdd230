7f00f995bebb1d33d951389565867aab13dff1f3
eehatch <hat19006@byui.edu>
2024-02-29 23:29:24 -0700

    home page form without css

---
+++ scripts/form.js
@@ -0,0 +1,51 @@
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#password2");
+const message = document.querySelector("#formmessage");
+
+confirmPassword.addEventListener("focusout", validatePassword);
+
+// This should be refactored.
+function checkSame() {
+	if (password.value !== confirmPassword.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		confirmPassword.style.backgroundColor = "#fff0f3";
+		confirmPassword.value = "";
+		confirmPassword.focus();
+	} else {
+		message.style.display = "none";
+		confirmPassword.style.backgroundColor = "#fff";
+		confirmPassword.style.color = "#000";
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	// Function to handle form submission
+	function handleSubmit(event) {
+		// Prevent default form submission
+		event.preventDefault();
+
+		// Access form elements
+		let formt = event.target;
+		let formData = new FormData(formt);
+
+		// Display form element values
+		for (let pair of formData.entries()) {
+			console.log(pair[0] + ": " + pair[1]);
+		}
+	}
+
+	const form = document.querySelector("form");
+	form.addEventListener("submit", handleSubmit);
+});
+
+const rangevalue = document.getElementById("rating");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
