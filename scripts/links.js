621560004e8403de58ce25796aa7cded2f108314
eehatch <hat19006@byui.edu>
2024-03-07 18:42:28 -0700

    added list to home page

--- scripts/links.js
+++ scripts/links.js
@@ -1,26 +1,38 @@
 const baseURL = "https://eehatch.github.io/wdd230/";
 const linksURL = "https://eehatch.github.io/wdd230/data/links.json";
-
-const lists = document.querySelector("#links");
+const lists = document.querySelector('#links');
 
 async function getLinks(){
     const response = await fetch(linksURL);
     const data = await response.json();
-    displayLinks(data);
+    displayLinks(data.lessons);
 }
-
 getLinks();
 
-const displayLinks = (lessons) => {
-    lessons.forEach((weeks) => {
-        let card = document.createElement('div');
-        let week = document.createElement('a');
-        let links = document.createElement( 'a' );
+const displayLinks = (weeks) => {
+    weeks.forEach((lesson) => {
+        const list = document.createElement('li');
 
-        week.textContent =  `Week ${weeks.lesson}: `; 
+        list.textContent = `Week ${lesson.lesson}: `;
+
+        lesson.links.forEach(function(week,index){
+            const link = document.createElement('a');
+            
+            link.textContent = `${week.title}`;
+            link.setAttribute('href', `${week.url}`)
+            link.setAttribute('target', `_blank`)
+
+            list.appendChild(link);
+
+            if (index < lesson.links.length -1 && list.childElementCount > 0) {
+                var space = document.createTextNode('|');
+                list.appendChild(space);
+            }
+        });
+
+        // let card = document.createElement('div');
+        // let week = document.createElement('a');
+
+        lists.appendChild(list);
     });
-
-    card.appendChild(week);
-    card.appendChild(links);
-    card.appendChild(card);
 }
